e534159c73d90c9864a3ff0a250b6e050210900b
chetan <chetankatwa12@gmail.com>
2024-01-11 23:13:46 +0530

    update

--- dist/config/constants.js
+++ dist/config/constants.js
@@ -1,9 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbCollections = exports.adminPassword = void 0;
-exports.adminPassword = "123@asd";
-exports.dbCollections = {
-    books: "books",
-    users: "users",
-    transactions: "transactions",
-};
